b9e4b31a0868f655ad1747c7061fc788e51e9cd0
Matvey Aliev <matvey.aliev@student.manchester.ac.uk>
2024-02-21 01:10:09 +0800

    Slightly change settings.py to fix loading secrets correctly and add them as env variables into GitHub workflow file

--- api-shop/shop/shop/settings.py
+++ api-shop/shop/shop/settings.py
@@ -40,10 +40,11 @@ elif os.environ.get("GOOGLE_CLOUD_PROJECT"):
     # Use Google Cloud Secrets if available
     print("Pulling secrets from Google Cloud Secret Manager")
     project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+    project_num = os.environ.get("GOOGLE_CLOUD_PROJECT_NUM")
     settings_name = os.environ.get("SETTINGS_NAME", "api-shop-settings")
 
     client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+    name = f"projects/{project_num}/secrets/{settings_name}/versions/latest"
     payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
     env.read_env(io.StringIO(payload))
 else:
