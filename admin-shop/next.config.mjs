3e378a9c48ee7b71b61d74db54df49c4ea6b1287
Matvey Aliev <matvey.aliev@student.manchester.ac.uk>
2024-04-13 19:00:07 +0800

    Create new next app for admin service

---
+++ admin-shop/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['static.tildacdn.com', 'files.edgestore.dev']
+  },
+  reactStrictMode: false,
+};
+
+export default nextConfig;
