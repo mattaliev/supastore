698d4fe08565002cd6cd43313c24aa5e6bbaffed
Matvey Aliev <matvey.aliev@student.manchester.ac.uk>
2024-04-15 18:33:39 +0800

    Extract state filter out of products filters as it can be reused

--- admin-shop/components/product/product-filters.tsx
+++ admin-shop/components/product/product-filters.tsx
@@ -1,66 +1,29 @@
 "use client";
 import { File, PlusCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Link from "next/link";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import StateFilter from "@/components/filters/state-filter";
 
 export default function ProductFilters() {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
-  let defaultValue;
-
-  if (!searchParams.get("state")) {
-    defaultValue = "all";
-  } else if (searchParams.get("state") === "ACTIVE") {
-    defaultValue = "active";
-  } else {
-    defaultValue = "draft";
-  }
-
-  const handleTabChange = (value: string) => {
-    const params = new URLSearchParams(searchParams);
-    if (value === "all") {
-      params.delete("state");
-    }
-
-    if (value === "active") {
-      params.set("state", "ACTIVE");
-    }
-
-    if (value === "draft") {
-      params.set("state", "INACTIVE");
-    }
-
-    replace(`${pathname}?${params.toString()}`);
-  };
-
   return (
-    <Tabs defaultValue={defaultValue} onValueChange={handleTabChange}>
-      <div className="flex items-center">
-        <TabsList>
-          <TabsTrigger value="all">All</TabsTrigger>
-          <TabsTrigger value="active">Active</TabsTrigger>
-          <TabsTrigger value="draft">Draft</TabsTrigger>
-        </TabsList>
-        <div className="ml-auto flex items-center gap-2">
-          <Button size="sm" variant="outline" className="h-8 gap-1">
-            <File className="h-3.5 w-3.5" />
+    <div className="flex items-center">
+      <StateFilter />
+      <div className="ml-auto flex items-center gap-2">
+        <Button size="sm" variant="outline" className="h-8 gap-1">
+          <File className="h-3.5 w-3.5" />
+          <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+            Import
+          </span>
+        </Button>
+        <Link href="/products/create">
+          <Button size="sm" className="h-8 gap-1">
+            <PlusCircle className="h-3.5 w-3.5" />
             <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-              Import
+              Add Product
             </span>
           </Button>
-          <Link href="/products/create">
-            <Button size="sm" className="h-8 gap-1">
-              <PlusCircle className="h-3.5 w-3.5" />
-              <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                Add Product
-              </span>
-            </Button>
-          </Link>
-        </div>
+        </Link>
       </div>
-    </Tabs>
+    </div>
   );
 }
