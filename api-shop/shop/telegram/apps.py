e6e5845a97c70716f99ab88c4a8806b65eefea06
Matvey Aliev <matvey.aliev@student.manchester.ac.uk>
2024-04-28 18:00:31 +0800

    Comment setting webhook out for now in telegram config

--- api-shop/shop/telegram/apps.py
+++ api-shop/shop/telegram/apps.py
@@ -1,20 +1,19 @@
-import logging
-
 from django.apps import AppConfig
 
-from telegram.services import telegram_shop_webhook_set, \
-    telegram_support_webhook_set
+
+# from telegram.services.core import telegram_shop_webhook_set, \
+#     telegram_support_webhook_set
 
 
 class TelegramConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'telegram'
 
-    def ready(self) -> None:
-        logger = logging.getLogger(self.__class__.__name__)
-
-        logger.debug("Setting up telegram shop webhook...")
-        telegram_shop_webhook_set()
-
-        logger.debug("Setting up telegram support webhook...")
-        telegram_support_webhook_set()
+    # def ready(self) -> None:
+    #     logger = logging.getLogger(self.__class__.__name__)
+    #
+    #     logger.debug("Setting up telegram shop webhook...")
+    #     telegram_shop_webhook_set()
+    #
+    #     logger.debug("Setting up telegram support webhook...")
+    #     telegram_support_webhook_set()
