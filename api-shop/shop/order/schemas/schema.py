5a0b6982063c85f70621cb3930d463c74179fc7b
Matvey Aliev <matvey.aliev@student.manchester.ac.uk>
2024-04-29 14:52:38 +0800

    Add changes to order type schema
    * Resolve payment
    * Resolve total_amount, subtotal_amount, shipping_amount
      - They are used in a lot of places on the frontend and if payment does not exist, it is pain in the arse to calculate them in different places
      - Only resolve them in a model to avoid duplicate data. Shouldn't affect performance as much because those are not large calculations. I highly doubt that someone will order a million items in one order

--- api-shop/shop/order/schemas/schema.py
+++ api-shop/shop/order/schemas/schema.py
@@ -32,11 +32,21 @@ class OrderType(DjangoObjectType):
     cart = graphene.Field('cart.schemas.CartType')
     has_default_shipping_details = graphene.Boolean()
     shipping = graphene.Field("shipping.schemas.ShippingType")
+    payment = graphene.Field("payment.schemas.schema.PaymentType")
     state = graphene.String()
+    shipping_amount = graphene.Decimal()
+    subtotal_amount = graphene.Decimal()
+    total_amount = graphene.Decimal()
+    fulfilment_status = graphene.String()
 
     class Meta:
         model = Order
-        fields = "__all__"
+        fields = [
+            "id", "order_number", "fulfilment_status", "fulfilment_date",
+            "user", "cart",
+            "has_default_shipping_details",
+            "shipping", "payment", "state", "created", "updated"
+        ]
 
     def resolve_has_default_shipping_details(self, info):
         if self.user is None:
@@ -45,7 +55,33 @@ class OrderType(DjangoObjectType):
         return self.user.has_default_shipping_details
 
     def resolve_shipping(self, info):
+        print(self.shipping)
         return self.shipping
+
+    def resolve_payment(self, info):
+        print(hasattr(self, "payment"))
+        if hasattr(self, "payment"):
+            return self.payment
+        return None
+
+    def resolve_shipping_amount(self, info):
+        if hasattr(self, "payment"):
+            return self.payment.shipping_amount
+
+        if self.shipping:
+            return self.shipping.shipping_amount
+
+    def resolve_subtotal_amount(self, info):
+        if hasattr(self, "payment"):
+            return self.payment.subtotal_amount
+
+        return self.cart.get_total_price()
+
+    def resolve_total_amount(self, info):
+        if hasattr(self, "payment"):
+            return self.payment.total_amount
+
+        return self.cart.get_total_price() + self.shipping.shipping_amount
 
 
 class OrderPaginatedType(PaginatedType):
