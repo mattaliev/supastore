bffe518187cb4ff719b15add877339f4959e9573
Matvey Aliev <matvey.aliev@student.manchester.ac.uk>
2024-04-29 15:25:22 +0800

    Refactor payment page to allow customer to see already created order
    * On the backend, there is a new inline button that allows customer to see their order again
    * That button will lead him to this page
    * But, we don't want user to try to change the order, because we want it to be immutable, so I refactored the shipping-summary to remove the opportunity for user to change their shipping details

---
+++ apps/tg-shop/components/checkout/shipping-summary.tsx
@@ -0,0 +1,59 @@
+import UpdateShippingDetailsButton
+  from "@/components/checkout/update-shipping-details-button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ShippingDetails } from "@ditch/lib";
+
+export const ShippingSummary = ({
+                                  shippingDetails,
+                                  mutable = true,
+                                }: {
+  shippingDetails?: ShippingDetails;
+  mutable?: boolean;
+}) => {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Contact information</CardTitle>
+        </CardHeader>
+        <CardContent className="text-sm flex items-center">
+          <dt className="grid gap-1 text-telegram-hint-color not-italic">
+            <dd>
+              {`${shippingDetails?.firstName} ${shippingDetails?.lastName}`}
+            </dd>
+            {/*<div className="text-telegram-hint-color">*/}
+            {/*  {shippingDetails?.phone || ""}*/}
+            {/*</div>*/}
+            {shippingDetails?.email && (
+              <dd>
+                {shippingDetails.email || ""}
+              </dd>
+            )}
+            {shippingDetails?.phone && (
+              <dd>
+                {shippingDetails.phone || ""}
+              </dd>
+            )}
+          </dt>
+          {mutable && <UpdateShippingDetailsButton />}
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Shipping address</CardTitle>
+        </CardHeader>
+        <CardContent
+          className="text-sm text-telegram-hint-color flex items-center">
+          <address className="text-telegram-hint-color not-italic">
+            <dd>{shippingDetails?.address || ""}</dd>
+            <dd>{shippingDetails?.city || ""}</dd>
+            <dd>{shippingDetails?.province || ""}</dd>
+            <dd>{shippingDetails?.postcode}</dd>
+            <dd>{shippingDetails?.country}</dd>
+          </address>
+          {mutable && <UpdateShippingDetailsButton />}
+        </CardContent>
+      </Card>
+    </>
+  );
+};
