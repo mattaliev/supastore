60e43dfeb98a738a12405d669c87a94ac22cf1f9
Matvey Aliev <matvey.aliev@student.manchester.ac.uk>
2024-05-02 11:48:53 +0800

    Small change in the url patterns
    * Accidentally had shop bot and support bot process the same update

--- api-shop/shop/telegram/urls.py
+++ api-shop/shop/telegram/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from telegram.views import telegram_shop_update
+from telegram.views import telegram_shop_update, telegram_support_update
 
 urlpatterns = [
     path("webhooks/shop/update/", telegram_shop_update,
          name="telegram_shop_update"),
-    path("webhooks/support/update/", telegram_shop_update,
+    path("webhooks/support/update/", telegram_support_update,
          name="telegram_support_update"),
 ]
