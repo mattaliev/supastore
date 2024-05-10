848eb104597fa38309f8288204826f4d7ed43439
Matvey Aliev <matvey.aliev@student.manchester.ac.uk>
2024-05-10 18:52:55 +0800

    Fix all the frontend build errors

--- apps/tg-shop/app/(shop)/checkout/shipping/page.tsx
+++ apps/tg-shop/app/(shop)/checkout/shipping/page.tsx
@@ -1,12 +1,17 @@
 import { orderGetById } from "@ditch/lib";
 import { cookies } from "next/headers";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 import ShippingDetailsForm from "@/components/checkout/shipping-details-form";
 import { tmaAuthenticated } from "@/lib/auth";
 
 export default async function ShippingDetailsPage() {
   const orderId = cookies().get("orderId")?.value;
+  const initDataRaw = cookies().get("initData")?.value;
+
+  if (!initDataRaw) {
+    redirect("/unauthenticated");
+  }
 
   if (!orderId) {
     return notFound();
