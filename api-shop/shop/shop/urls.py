f7fa5ea7dfafa7858536855927c45a96be7acb28
Matvey Aliev <matvey.aliev@student.manchester.ac.uk>
2024-05-24 22:20:33 +0800

    Update settings.py and urls
    Signed-off-by: Matvey Aliev <matvey.aliev@student.manchester.ac.uk>

--- api-shop/shop/shop/urls.py
+++ api-shop/shop/shop/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
+from graphene_file_upload.django import FileUploadGraphQLView
 
 from core.views import index
 
@@ -10,5 +10,5 @@ urlpatterns = [
     path("", index, name="index"),
     path("payment/", include("payment.urls"), name="payment"),
     path("telegram/", include("telegram.urls"), name="telegram"),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True)))
+    path('graphql/', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True)))
 ]
