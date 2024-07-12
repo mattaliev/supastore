60e0623b971bdf9a3ecaafae99ba3c56deddeded
Matvey Aliev <matvey.aliev@student.manchester.ac.uk>
2024-07-12 15:09:44 +0800

    Implement service function for importing products from Wildberries
    * Accepts wildberries API token and overwrites existing products
    * Gets all products from Wildberries through API with prices
    * Upload product images to Google cloud storage
    * Save all product data in db
    
    Signed-off-by: Matvey Aliev <matvey.aliev@student.manchester.ac.uk>

--- api-shop/shop/product/services/product_services.py
+++ api-shop/shop/product/services/product_services.py
@@ -1,7 +1,14 @@
+import hashlib
 import logging
 from typing import List
 from uuid import UUID
 
+import requests
+from django.conf import settings
+from google.cloud import storage
+
+from apps.apis import WbAPI
+from category.models import Category, Characteristic
 from core.exceptions import NotFoundError, ServerError
 from core.models import EntityStateChoices, Image
 from product.models import (
@@ -301,3 +308,165 @@ def product_variants_order_set(product_ids: List[UUID]):
 
     for idx, variant_id in enumerate(product_ids):
         ProductVariant.objects.filter(id=variant_id).update(order_display=idx)
+
+
+def product_variant_import_from_wb(
+        *,
+        store_id: UUID,
+        wb_api_token: str,
+        overwrite_existing_products: bool = False
+):
+    logger = logging.getLogger(__name__)
+    logger.debug("Fetching product variants from Wildberries")
+
+    wb_api = WbAPI(wb_api_token)
+    wb_products = wb_api.get_all_products()
+    wb_sizes = wb_api.get_all_product_sizes()
+
+    bulk_products_from_wb_create(
+        store_id=store_id,
+        products=wb_products,
+        sizes=wb_sizes,
+        overwrite_existing_products=overwrite_existing_products
+    )
+
+
+def bulk_products_from_wb_create(
+        *,
+        store_id: UUID,
+        products: list[dict],
+        sizes: list[dict],
+        overwrite_existing_products: bool = False
+):
+    logger = logging.getLogger(__name__)
+    logger.debug("Creating products from Wildberries")
+
+    for wb_product in products:
+        product_wb_id = wb_product["imtID"]
+        product_variant_wb_id = wb_product["nmID"]
+        product_variant = ProductVariant.objects.filter(wb_id=product_variant_wb_id).first()
+
+        if product_variant and not overwrite_existing_products:
+            continue
+
+        product = Product.objects.filter(wb_id=product_wb_id).first()
+
+        if not product:
+            category = Category.objects.filter(wb_id=wb_product["subjectID"]).first()
+            product, _ = Product.objects.create(
+                wb_id=product_wb_id,
+                category=category,
+                store_id=store_id
+            )
+
+        product_variant, _ = ProductVariant.objects.update_or_create(
+            wb_id=product_variant_wb_id,
+            defaults={
+                "product": product,
+                "store_id": store_id,
+                "name": wb_product["title"],
+                "description": wb_product["description"],
+                "brand": wb_product["brand"],
+                "sku": wb_product["vendorCode"]
+            }
+        )
+
+        product_variant_images_from_wb_create(product_variant=product_variant, images_dict=wb_product["photos"])
+
+        sizes_with_price = filter(lambda size: size["nmID"] == product_variant_wb_id, sizes)
+        wb_product_sizes = wb_product["sizes"]
+
+        product_variant_sizes_from_wb_create(
+            product_variant=product_variant,
+            sizes=wb_product_sizes,
+            sizes_with_price=sizes_with_price
+        )
+
+        product_variant_characteristics_from_wb_create(product_variant=product_variant, characteristics=wb_product["characteristics"])
+
+
+def product_variant_images_from_wb_create(*, product_variant: ProductVariant, images_dict: list[dict]):
+    logger = logging.getLogger(__name__)
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(settings.GS_BUCKET_NAME)
+
+    for idx, image in enumerate(images_dict):
+        try:
+            response = requests.get(image["big"])
+            response.raise_for_status()
+
+            hash_object = hashlib.sha256(image["big"].encode())
+            image_url_hash = hash_object.hexdigest()
+
+            image_data = response.content
+            image_extension = image["big"].split(".")[-1]
+            destination_blob_name = (
+                f"store/{product_variant.store_id}/"
+                f"product_images/{product_variant.id}"
+                f"/{image_url_hash}.{image_extension}"
+            )
+
+            blob = bucket.blob(destination_blob_name)
+            blob.upload_from_string(image_data, content_type=response.headers["Content-Type"])
+
+            blob.make_public()
+
+            product_variant_image_create(
+                product_variant_id=product_variant.id,
+                image_url=blob.public_url,
+                order=idx
+            )
+
+        except Exception as e:
+            logger.warning("Error uploading image: %s with error: %e", image["big"])
+            continue
+
+
+def product_variant_sizes_from_wb_create(
+        *,
+        product_variant: ProductVariant,
+        sizes: list[dict],
+        sizes_with_price: list[dict]
+):
+    logger = logging.getLogger(__name__)
+
+    for size in sizes:
+        price = next(
+            filter(
+                lambda size_with_price: size_with_price["sizeID"] == size["chrtID"]
+                , sizes_with_price
+            ), None
+        )
+
+        if not price:
+            logger.warning("Could not find price for size: %s", size["chrtID"])
+            continue
+
+        ProductVariantSize.objects.update_or_create(
+            wb_id=size["chrtId"],
+            defaults={
+                "product_variant": product_variant,
+                "size_en": size["techSize"],
+                "size_ru": size["wbSize"],
+                "price": price["price"]
+            }
+        )
+
+
+def product_variant_characteristics_from_wb_create(*, product_variant: ProductVariant, wb_characteristics: list[dict]):
+    for wb_characteristic in wb_characteristics:
+        characteristic = Characteristic.objects.filter(wb_id=wb_characteristic["id"]).first()
+
+        if not characteristic:
+            continue
+
+        ProductVariantCharacteristics.objects.update_or_create(
+            product_variant=product_variant,
+            characteristic=characteristic,
+            defaults={
+                "value": wb_characteristic["value"]
+            }
+        )
+
+
+
